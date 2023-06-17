1c0fa1b2f661406e8fe63d5bcf695bad9e2a8c7a
metalmessenger <101930270+metalmessenger@users.noreply.github.com>
2023-06-17 23:57:15 +0900

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -10,7 +10,7 @@ from tiktokapipy.async_api import AsyncTikTokAPI
 # ghPagesURL = "https://conoro.github.io/tiktok-rss-flat/"
 
 # Custom Domain
-ghPagesURL = "https://metalmessenger.github.io/tiktok-rss-flat/"
+ghPagesURL = "https://metalmessenger.github.io/"
 
 maxItems = 5
 
